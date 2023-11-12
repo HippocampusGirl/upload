028864a13785fd824bfdad209f57f4f588145422
Lea Waller <lea@lea.science>
2023-11-12 18:27:52 +0100

    Raise EPERM instead of killing the process in systemd system call filter

--- module.nix
+++ module.nix
@@ -78,6 +78,7 @@ in {
         RestrictRealtime = true;
         RestrictSUIDSGID = true;
         SystemCallArchitectures = "native";
+        SystemCallErrorNumber = "EPERM";
         SystemCallFilter = [
           "@basic-io"
           "@network-io"
