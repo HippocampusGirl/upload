45e6a4cdc9485dbfa48c2b154668952521327319
Lea Waller <lea.waller@charite.de>
2023-11-15 20:20:14 +0100

    Add proxy

---
+++ src/proxy.ts
@@ -0,0 +1,20 @@
+import { HttpsProxyAgent } from "https-proxy-agent";
+import Joi from "joi";
+import { Agent } from "node:https";
+
+const httpProxy =
+  process.env.http_proxy ||
+  process.env.HTTP_PROXY ||
+  process.env.https_proxy ||
+  process.env.HTTPS_PROXY ||
+  process.env.all_proxy ||
+  process.env.ALL_PROXY;
+export const httpProxySchema = Joi.string().uri({
+  scheme: ["http", "https"],
+});
+export const getHttpsProxyAgent = (): Agent | undefined => {
+  if (httpProxy) {
+    Joi.assert(httpProxy, httpProxySchema);
+    return new HttpsProxyAgent(httpProxy);
+  }
+};
--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,14 +1,26 @@
 import Debug from "debug";
-import { io } from "socket.io-client";
+import Joi from "joi";
+import { io, ManagerOptions, SocketOptions } from "socket.io-client";
 
+import { getHttpsProxyAgent } from "./proxy.js";
 import { _ClientSocket } from "./socket.js";
 
 const debug = Debug("socket-client");
 
+export const endpointSchema = Joi.string().uri({
+  scheme: ["http", "https", "ws", "wss"],
+});
 export const makeClient = (endpoint: string, token: string): _ClientSocket => {
-  const socket = io(endpoint, {
+  const options: Partial<ManagerOptions & SocketOptions> = {
     auth: { token },
-  });
+  };
+
+  const agent = getHttpsProxyAgent();
+  if (agent !== undefined) {
+    options.agent = agent as any;
+  }
+
+  const socket = io(endpoint, options);
 
   socket.on("connect_error", (error) => {
     debug(`Failed to connect to server: ${error.message}`);
