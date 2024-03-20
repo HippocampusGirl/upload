c9a13845b0b4629f5c5a82206ec62b7f4def9820
Lea Waller <lea.waller@charite.de>
2024-03-20 13:09:11 +0100

    0.2.5

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@ pkgs.buildNpmPackage {
   nativeBuildInputs = with pkgs; [ python3 ];
   nodejs = pkgs.nodejs_21;
 
-  npmDepsHash = "sha256-vwBuRjORahEthkTx2IGnaxMWNpg6lbtlH2gkCAkQdlo=";
+  npmDepsHash = "sha256-sN0ZdyK51t1HxGy576CdHYbQI4FyeDUAankkB6JBh+Y=";
 
   meta = with lib; {
     description = "A software for data transfers via the cloud";
