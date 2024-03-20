e1d0c7dd2684274aacce62023d4383c2b2e025e8
Lea Waller <lea.waller@charite.de>
2024-03-20 12:35:38 +0100

    Add pre-commit hook to ensure npmDepsHash stays up to date

---
+++ bin/default-nix-update-npm-deps-hash.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+set -x
+set -e
+
+npm_deps_hash=$(nix-shell --packages prefetch-npm-deps --run "prefetch-npm-deps $1")
+sed -i "s#npmDepsHash = .*#npmDepsHash = \"${npm_deps_hash}\";#g" default.nix
